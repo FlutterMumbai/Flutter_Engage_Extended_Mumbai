345877218a3a9d0ec134171b88fdca612fba545a
Aditya Sutar <aditya.sutar305@gmail.com>
2021-02-21 17:33:24 +0530

    Create assets.dart

---
+++ lib/utils/assets.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Assets {
+  static const String flutterBlue = "assets/png/Flutter_Logo_blue.png";
+  static const String flutterCity = "assets/png/flutter_city.png";
+  static const String flutetrCity1 = "assets/png/flutter_city_2.png";
+  static const String liquidArt = "assets/png/liquid_art.png";
+  static const String flutterMumbai = "assets/png/flutter_mumbai_logo.png";
+  static const String googleDevelopers = "assets/png/google_developer_logo.png";
+  static const String jetBrain = "assets/png/jetbrains.png";
+  static const String flutterLogo = "assets/png/logo_lockup_flutter_horizontal.png";
+
+  // Social Icon
+  static const String facebook = "assets/icons/social/facebook.png";
+  static const String twitter = "assets/icons/social/twitter.png";
+  static const String youTube = "assets/icons/social/youtube.png";
+  static const String teleGram = "assets/icons/social/telegram.png";
+  static const String lingkdIn = "assets/icons/social/linkdin.png";
+}
