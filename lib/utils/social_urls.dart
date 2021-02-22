4f7c2cf0c42665f3ffbac2e61d925de1ef1a7c31
Aditya Sutar <aditya.sutar305@gmail.com>
2021-02-22 16:29:30 +0530

    Create social_urls.dart

---
+++ lib/utils/social_urls.dart
@@ -0,0 +1,21 @@
+
+
+class SocialUrls {
+  static const String facebook = "https://www.facebook.com/fluttermumbai";
+  static const String twitter = "https://twitter.com/MumbaiFlutter";
+  static const String instagram = "https://www.instagram.com/mumbaiflutter/";
+  static const String telegram = "https://t.me/FlutterMumbai";
+  static const String meetup = "https://www.meetup.com/Mumbai-Flutter/";
+  static const String youtube =
+      "https://www.youtube.com/channel/UCpC1LShsSM7iYMBYLNEerxg?sub_confirmation=1";
+  static const String linkdIn =
+      "https://www.linkedin.com/company/fluttermumbai";
+  static const String github = "https://github.com/FlutterMumbai";
+  static const String communityGuildline = "https://events.flutter.dev/engage/guidelines";
+  static const String rsvpUrl = "";
+
+  // hash tag  urls
+static const String engageTagUrl = "https://twitter.com/search?q=%23FlutterEngage";
+static const String flutterMumTagUrl = "https://twitter.com/search?q=%23FlutterMumbai";
+static const String askFlutterMumUrl = "https://twitter.com/search?q=%23AskFlutterMumbai";
+}
