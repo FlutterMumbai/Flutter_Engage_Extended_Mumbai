b3535f45d1924723b8f806bc25a4ab9e6d22e71d
Aditya Sutar <aditya.sutar305@gmail.com>
2021-02-22 16:28:54 +0530

    Update event_schedule_page.dart

--- lib/pages/event_schedule_page.dart
+++ lib/pages/event_schedule_page.dart
@@ -1,41 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:flutterengagemumbai/utils/assets.dart';
+import 'package:flutterengagemumbai/services/responsiveness.dart';
+
 
 class EventSchedulePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Event Schedule",
-              style: Theme.of(context).textTheme.headline3,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "To be announce soon",
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            SizedBox(
-              height: 70.0,
-            ),
-            Flexible(
-              child: Image.asset(
-                Assets.liquidArt,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.fitWidth,
-                filterQuality: FilterQuality.high,
+    return eventSchedulePageContext(context);
+  }
+
+  Container eventSchedulePageContext(BuildContext context) {
+    if (Responsiveness.isLargeScreen(context)) {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Event Schedule",
+                style: Theme.of(context).textTheme.headline3,
               ),
-            ),
-          ],
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "To be announce soon",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(
+                height: 70.0,
+              ),
+              Flexible(
+                child: Image.asset(
+                  Assets.liquidArt,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.fitWidth,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } else if (Responsiveness.isMediumScreen(context)) {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 1.7,
+        padding: EdgeInsets.symmetric(vertical: 10.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Event Schedule",
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "To be announce soon",
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              SizedBox(
+                height: 70.0,
+              ),
+              Flexible(
+                child: Image.asset(
+                  Assets.liquidArt,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.fitWidth,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height / 1.5,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Event Schedule",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "To be announce soon",
+                style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                  fontSize: 15.0,
+                ),
+              ),
+              SizedBox(
+                height: 70.0,
+              ),
+              Flexible(
+                child: Image.asset(
+                  Assets.liquidArt,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.fitWidth,
+                  filterQuality: FilterQuality.high,
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
