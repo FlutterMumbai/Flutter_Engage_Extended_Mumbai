2a23709b5b07b7f27dae24e89c80d004c0748a95
Aditya Sutar <aditya.sutar305@gmail.com>
2021-03-20 15:04:12 +0530

    Update sponser_page.dart

--- lib/pages/sponser_page.dart
+++ lib/pages/sponser_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutterengagemumbai/utils/assets.dart';
 import 'package:flutterengagemumbai/services/responsiveness.dart';
+import 'package:flutterengagemumbai/utils/assets.dart';
 
 class SponserPage extends StatelessWidget {
   @override
@@ -28,22 +28,30 @@ class SponserPage extends StatelessWidget {
                   Image.asset(
                     Assets.flutterLogo,
                     scale: 7.0,
+                    filterQuality: FilterQuality.high,
                   ),
                   Image.asset(
                     Assets.googleDevelopers,
+                    filterQuality: FilterQuality.high,
                   ),
                   Image.asset(
                     Assets.jetBrain,
                     scale: 9.0,
+                    filterQuality: FilterQuality.high,
                   ),
                 ],
+              ),
+              SizedBox(height: 10.0),
+              Image.asset(
+                Assets.theCompanyLogo,
+                scale: 3.0,
+                filterQuality: FilterQuality.high,
               ),
             ],
           ),
         ),
       );
-    }
-    else if(Responsiveness.isMediumScreen(context)) {
+    } else if (Responsiveness.isMediumScreen(context)) {
       return Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height / 1.3,
@@ -58,22 +66,30 @@ class SponserPage extends StatelessWidget {
             Image.asset(
               Assets.flutterLogo,
               scale: 8.0,
+              filterQuality: FilterQuality.high,
             ),
             SizedBox(height: 50.0),
             Image.asset(
               Assets.googleDevelopers,
               scale: 1.0,
+              filterQuality: FilterQuality.high,
             ),
             SizedBox(height: 50.0),
             Image.asset(
               Assets.jetBrain,
               scale: 9.0,
+              filterQuality: FilterQuality.high,
+            ),
+            SizedBox(height: 50.0),
+            Image.asset(
+              Assets.theCompanyLogo,
+              scale: 3.0,
+              filterQuality: FilterQuality.high,
             ),
           ],
         ),
       );
-    }
-    else {
+    } else {
       return Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
@@ -88,16 +104,25 @@ class SponserPage extends StatelessWidget {
             Image.asset(
               Assets.flutterLogo,
               scale: 12.0,
+              filterQuality: FilterQuality.high,
             ),
-           SizedBox(height: 40.0),
+            SizedBox(height: 40.0),
             Image.asset(
               Assets.googleDevelopers,
               scale: 1.7,
+              filterQuality: FilterQuality.high,
             ),
             SizedBox(height: 40.0),
             Image.asset(
               Assets.jetBrain,
               scale: 17.0,
+              filterQuality: FilterQuality.high,
+            ),
+            SizedBox(height: 40.0),
+            Image.asset(
+              Assets.theCompanyLogo,
+              scale: 5.0,
+              filterQuality: FilterQuality.high,
             ),
           ],
         ),
